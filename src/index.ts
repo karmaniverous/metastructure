6e43088e5955ffa596f08ec42b0e4a54f1397419
Jason Williscroft <me@karmanivero.us>
2024-07-16 19:04:41 +0800

    lintfix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { applyLicenseHeaders } from './applyLicenseHeaders';
 export { type Config } from './Config';
-export { type RC } from './configFile';
 export { formatFiles } from './formatFiles';
-export { parseConfig } from './parseConfig';
 export { generateBatch } from './generateBatch';
+export { parseConfig } from './parseConfig';
 export { updateConfig } from './updateConfig';
